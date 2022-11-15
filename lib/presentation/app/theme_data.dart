15f16f29ff649acd38892b6e629c11817be9ef99
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2022-11-16 00:23:33 +0300

    Implements NotoSans font

--- lib/presentation/app/theme_data.dart
+++ lib/presentation/app/theme_data.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:animations/animations.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pokedex/presentation/ui/resources/colors.dart';
 
 final themeData = ThemeData(
+  textTheme: GoogleFonts.notoSansTextTheme(),
   pageTransitionsTheme: const PageTransitionsTheme(
     builders: {
       TargetPlatform.android: SharedAxisPageTransitionsBuilder(
