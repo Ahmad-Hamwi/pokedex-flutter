e73f1e82bd652af4e648b45d1c0fbd18dc293a25
Ahmad Hamwi <ahmadhamwi999@gmail.com>
2023-07-17 01:05:40 +0300

    Fixes a bug within toggle_pokemon_favourite_test.dart not arranging test properly

--- test/unit/domain/interactor/toggle_pokemon_favourite_test.dart
+++ test/unit/domain/interactor/toggle_pokemon_favourite_test.dart
@@ -11,38 +11,41 @@ void main() {
   late MockPokemonEntity toggledMock;
   late MockPokemonEntity savedMock;
 
+  late TogglePokemonFavouriteUseCase sut;
+
   setUp(() {
     repoMock = MockIPokemonRepository();
     toBeToggledMock = MockPokemonEntity();
     toggledMock = MockPokemonEntity();
     savedMock = MockPokemonEntity();
+
+    sut = TogglePokemonFavouriteUseCase(repoMock);
   });
 
   group("TogglePokemonFavouriteUseCase", () {
-    when(repoMock.savePokemon(toggledMock))
-        .thenAnswer((_) => Future.value(savedMock));
-
-    final useCaseActual = TogglePokemonFavouriteUseCase(repoMock);
-
     test("Makes an `infavourite` pokemon as `favourite`", () async {
+      when(repoMock.savePokemon(toggledMock))
+          .thenAnswer((_) => Future.value(savedMock));
       when(toBeToggledMock.isFavourite).thenReturn(false);
       when(toggledMock.isFavourite).thenReturn(true);
       when(toBeToggledMock.copyWith(isFavourite: true)).thenReturn(toggledMock);
       when(savedMock.isFavourite).thenReturn(true);
 
-      final toggledFavPokemon = await useCaseActual.execute(toBeToggledMock);
+      final toggledFavPokemon = await sut.execute(toBeToggledMock);
 
       expect(toggledFavPokemon.isFavourite, true);
     });
 
     test("Makes a `favourite` pokemon as `infavourite`", () async {
+      when(repoMock.savePokemon(toggledMock))
+          .thenAnswer((_) => Future.value(savedMock));
       when(toBeToggledMock.isFavourite).thenReturn(true);
       when(toggledMock.isFavourite).thenReturn(false);
       when(toBeToggledMock.copyWith(isFavourite: false))
           .thenReturn(toggledMock);
       when(savedMock.isFavourite).thenReturn(false);
 
-      final toggledFavPokemon = await useCaseActual.execute(toBeToggledMock);
+      final toggledFavPokemon = await sut.execute(toBeToggledMock);
 
       expect(toggledFavPokemon.isFavourite, false);
     });
